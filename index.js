c31d7552bbe49680c50ba8a4b93c0e21774d16e4
William (Wgyt) Horning <github@wgyt.tk>
2021-02-23 16:47:22 +0000

    fixd

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const fs = require("fs-extra")
 const _$ = require("bijou.js")
 module.exports = function(host, res, req, error, version, ejs){
 	path = req.originalUrl
-	res.send(_$.markdownToHTML(fs.readFileSync("page.md")))
+	res.send(_$.markdownToHTML(fs.readFileSync(`${__dirname}/page.md`)))
 }
--- page.md
+++ page.md
@@ -1,1 +1,2 @@
 # big funnies are really funny so heres a funny
+## if my hair is pink, my hair is pink :
